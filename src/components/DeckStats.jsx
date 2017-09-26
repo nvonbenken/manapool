56b993e8a3fa7b82054b549ce273fe9a6b98e7ef
Nate Von Benken <nvonbenken@gmail.com>
2017-09-25 20:28:03 -0400

    Initial graph work

--- src/components/DeckStats.jsx
+++ src/components/DeckStats.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Accordion, Button } from 'semantic-ui-react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 class DeckList extends Component {
   constructor(props) {
@@ -11,7 +12,7 @@ class DeckList extends Component {
   }
 
   render() {
-    console.log(this.props.deck);
+    const data = Array.from(this.props.deck.values());
 
     if (!this.props.deck || this.props.deck.size === 0) {
       return (
@@ -22,6 +23,8 @@ class DeckList extends Component {
       );
     }
 
+    console.log(data);
+
     return (
       <div
         style={{
@@ -30,7 +33,35 @@ class DeckList extends Component {
           overflow: 'auto',
         }}
       >
-        <h4>Deck Stats</h4>
+        <h4>Mana Curve:</h4>
+        <BarChart
+          width={400}
+          height={200}
+          data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <XAxis dataKey="card.cmc" />
+          <YAxis allowDecimals={false} padding={{ top: 10 }} />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="quantity" fill="#82ca9d" />
+        </BarChart>
+        <h4>Card Distribution:</h4>
+        <h4>Color Distribution:</h4>
+        <BarChart
+          width={400}
+          height={200}
+          data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <XAxis dataKey="card.colors" />
+          <YAxis allowDecimals={false} padding={{ top: 10 }} />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="quantity" fill="#82ca9d" />
+        </BarChart>
       </div>
     );
   }
