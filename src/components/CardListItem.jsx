e55b1317f7f709e4e013fa49ec858f4a4211944c
nvonbenken <nvonbenken@gmail.com>
2017-10-25 22:15:46 -0400

    Adding progressive image loading

--- src/components/CardListItem.jsx
+++ src/components/CardListItem.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import ProgressiveImage from 'react-progressive-image';
 import '../styles/cardList.css';
 
 const CardListItem = ({ card, onCardSelect }) => (
   <div onClick={event => onCardSelect(card)} className="card">
     <div>
       <div>
-        <img
-          style={{ borderRadius: '10px', width: '223px', height: '310px' }}
-          src={card.imageUrl}
-          alt=""
-        />
+        <ProgressiveImage src={card.imageUrl}>
+          {src => (
+            <img
+              src={src}
+              alt=""
+              style={{ borderRadius: '10px', width: '223px', height: '310px' }}
+            />
+          )}
+        </ProgressiveImage>
       </div>
     </div>
   </div>
