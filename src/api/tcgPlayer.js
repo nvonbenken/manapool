b620f5ea408fa98fef24dcf45edced5b001414bd
Unknown <nvonbenken@gmail.com>
2018-02-19 20:45:34 -0500

    Further work to clean this up

--- src/api/tcgPlayer.js
+++ src/api/tcgPlayer.js
@@ -1,84 +1,82 @@
-const accessToken = "";
+const accessToken = '';
 
 export function GetAccessToken() {
-  return fetch(
-    "https://cors-anywhere.herokuapp.com/https://api.tcgplayer.com/token",
-    {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "cache-control": "no-cache"
-      },
-      method: "POST",
-      body:
-        "grant_type=client_credentials&client_id=0A436DDE-5EB7-4B14-881B-3971A625B541&client_secret=62EF4907-FD23-445A-A586-0581DDBE4CB3"
-    }
-  )
+  return fetch('https://cors-anywhere.herokuapp.com/https://api.tcgplayer.com/token', {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'cache-control': 'no-cache',
+    },
+    method: 'POST',
+    body:
+      'grant_type=client_credentials&client_id=0A436DDE-5EB7-4B14-881B-3971A625B541&client_secret=62EF4907-FD23-445A-A586-0581DDBE4CB3',
+  })
     .then(response => response.json())
-    .then(responseData => {
-      return responseData;
-    })
+    .then(responseData => responseData)
     .catch(error => console.warn(error));
 }
 
-const getProductIds = card => {
+export function GetProductIds(card) {
   const data = {
     offset: 0,
     limit: 10,
     includeAggregates: true,
     filters: [
       {
-        name: "ProductName",
-        values: [card.name]
+        name: 'ProductName',
+        values: [card.name],
       },
       {
-        name: "SetName",
-        values: [card.setName]
-      }
-    ]
+        name: 'SetName',
+        values: [card.setName],
+      },
+    ],
   };
 
   return fetch(
-    "https://cors-anywhere.herokuapp.com/http://api.tcgplayer.com/v1.6.0/catalog/categories/1/search",
+    'https://cors-anywhere.herokuapp.com/http://api.tcgplayer.com/v1.6.0/catalog/categories/1/search',
     {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        Authorization: `bearer ${accessToken}`
+        'Content-Type': 'application/json',
+        Authorization: `bearer ${accessToken}`,
       },
-      body: JSON.stringify(data)
-    }
-  ).then(response => response.json());
-};
+      body: JSON.stringify(data),
+    },
+  )
+    .then(response => response.json())
+    .then(responseData => responseData)
+    .catch(error => console.warn(error));
+}
 
-const getProductInfo = ids =>
-  fetch(
+export function GetProductInfo(ids) {
+  return fetch(
     `https://cors-anywhere.herokuapp.com/http://api.tcgplayer.com/v1.5.0/catalog/products/${ids.join()}`,
     {
-      method: "GET",
+      method: 'GET',
       headers: {
-        "Content-Type": "application/json",
-        Authorization: `bearer ${accessToken}`
-      }
-    }
+        'Content-Type': 'application/json',
+        Authorization: `bearer ${accessToken}`,
+      },
+    },
   )
     .then(response => response.json())
-    .then(responseJson => {
-      console.log(responseJson);
-    });
+    .then(responseData => responseData)
+    .catch(error => console.warn(error));
+}
 
-const getProductMarketPrice = ids =>
-  fetch(
+export function GetProductMarketPrice(ids) {
+  return fetch(
     `https://cors-anywhere.herokuapp.com/http://api.tcgplayer.com/v1.6.0/pricing/product/${ids.join()}`,
     {
-      method: "GET",
+      method: 'GET',
       headers: {
-        "Content-Type": "application/json",
-        Authorization: `bearer ${accessToken}`
-      }
-    }
+        'Content-Type': 'application/json',
+        Authorization: `bearer ${accessToken}`,
+      },
+    },
   )
     .then(response => response.json())
-    .then(responseJson => {
-      return responseJson.results[0];
-    });
+    .then(responseData => responseData.results[0])
+    .catch(error => console.warn(error));
+}
--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
-import { Table, Divider, Button, Icon, Popup } from "semantic-ui-react";
+import React, { Component } from 'react';
+import { Table, Divider, Button, Icon, Popup } from 'semantic-ui-react';
+import { GetProductIds, GetProductMarketPrice } from '../api/tcgPlayer';
 
-import "../styles/cardDetail.css";
+import '../styles/cardDetail.css';
 
 class CardDetail extends Component {
   constructor(props) {
@@ -11,85 +12,23 @@ class CardDetail extends Component {
       cost: null,
       page: 0,
       loading: true,
-      favorite: false
+      favorite: false,
     };
   }
-
-  getProductIds = () => {
-    const data = {
-      offset: 0,
-      limit: 10,
-      includeAggregates: true,
-      filters: [
-        {
-          name: "ProductName",
-          values: [this.props.cardArr[this.state.page].name]
-        },
-        {
-          name: "SetName",
-          values: [this.props.cardArr[this.state.page].setName]
-        }
-      ]
-    };
-
-    return fetch(
-      "https://cors-anywhere.herokuapp.com/http://api.tcgplayer.com/v1.6.0/catalog/categories/1/search",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `bearer ${this.props.accessToken}`
-        },
-        body: JSON.stringify(data)
-      }
-    ).then(response => response.json());
-  };
-
-  getProductInfo = ids =>
-    fetch(
-      `https://cors-anywhere.herokuapp.com/http://api.tcgplayer.com/v1.5.0/catalog/products/${ids.join()}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `bearer ${this.props.accessToken}`
-        }
-      }
-    )
-      .then(response => response.json())
-      .then(responseJson => {
-        console.log(responseJson);
-      });
-
-  getProductMarketPrice = ids =>
-    fetch(
-      `https://cors-anywhere.herokuapp.com/http://api.tcgplayer.com/v1.6.0/pricing/product/${ids.join()}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `bearer ${this.props.accessToken}`
-        }
-      }
-    )
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState({ cost: responseJson.results[0] });
-      });
-
-  handleClick = (event, type) => {
-    this.setState({ loading: true });
-    this.setState({ cost: null });
-    if (type === "next") {
-      this.setState({ page: this.state.page + 1 });
-    } else if (type === "previous") {
-      this.setState({ page: this.state.page - 1 });
-    }
-  };
 
   setFavorite = () => {
     console.log(this.props.cardArr[this.state.page]);
     this.setState({ favorite: !this.state.favorite });
+  };
+
+  handleClick = (event, type) => {
+    this.setState({ loading: true });
+    this.setState({ cost: null });
+    if (type === 'next') {
+      this.setState({ page: this.state.page + 1 });
+    } else if (type === 'previous') {
+      this.setState({ page: this.state.page - 1 });
+    }
   };
 
   render() {
@@ -99,7 +38,7 @@ class CardDetail extends Component {
 
     let rulings = null;
 
-    if (typeof this.props.cardArr[this.state.page].rulings === "undefined") {
+    if (typeof this.props.cardArr[this.state.page].rulings === 'undefined') {
       rulings = (
         <Table celled>
           <Table.Header>
@@ -131,7 +70,7 @@ class CardDetail extends Component {
           <Table.Body>
             {this.props.cardArr[this.state.page].rulings.map(item => (
               <Table.Row key={item.multiverseid}>
-                <Table.Cell style={{ width: "100px" }}>{item.date}</Table.Cell>
+                <Table.Cell style={{ width: '100px' }}>{item.date}</Table.Cell>
                 <Table.Cell>{item.text}</Table.Cell>
               </Table.Row>
             ))}
@@ -142,7 +81,7 @@ class CardDetail extends Component {
 
     let legalities = null;
 
-    if (typeof this.props.cardArr[this.state.page].legalities === "undefined") {
+    if (typeof this.props.cardArr[this.state.page].legalities === 'undefined') {
       legalities = (
         <Table celled>
           <Table.Header>
@@ -174,7 +113,7 @@ class CardDetail extends Component {
           <Table.Body>
             {this.props.cardArr[this.state.page].legalities.map(item => (
               <Table.Row key={item.multiverseid}>
-                <Table.Cell style={{ width: "100px" }}>
+                <Table.Cell style={{ width: '100px' }}>
                   {item.format}
                 </Table.Cell>
                 <Table.Cell>{item.legality}</Table.Cell>
@@ -219,7 +158,7 @@ class CardDetail extends Component {
       );
     } else {
       cost = (
-        <div style={{ width: "180px" }}>
+        <div style={{ width: '180px' }}>
           <Table celled>
             <Table.Header>
               <Table.Row>
@@ -234,15 +173,15 @@ class CardDetail extends Component {
             </Table.Header>
             <Table.Body>
               <Table.Row key={this.state.cost.productId}>
-                <Table.Cell style={{ width: "100px" }}>Low Price</Table.Cell>
+                <Table.Cell style={{ width: '100px' }}>Low Price</Table.Cell>
                 <Table.Cell>${this.state.cost.lowPrice.toFixed(2)}</Table.Cell>
               </Table.Row>
               <Table.Row key={this.state.cost.productId}>
-                <Table.Cell style={{ width: "100px" }}>Mid Price</Table.Cell>
+                <Table.Cell style={{ width: '100px' }}>Mid Price</Table.Cell>
                 <Table.Cell>${this.state.cost.midPrice.toFixed(2)}</Table.Cell>
               </Table.Row>
               <Table.Row key={this.state.cost.productId}>
-                <Table.Cell style={{ width: "100px" }}>High Price</Table.Cell>
+                <Table.Cell style={{ width: '100px' }}>High Price</Table.Cell>
                 <Table.Cell>${this.state.cost.highPrice.toFixed(2)}</Table.Cell>
               </Table.Row>
             </Table.Body>
@@ -259,9 +198,9 @@ class CardDetail extends Component {
       this.state.cost === null &&
       this.state.loading
     ) {
-      this.getProductIds().then(results => {
+      GetProductIds(this.props.cardArr[this.state.page]).then((results) => {
         if (results.totalItems > 0) {
-          this.getProductMarketPrice(results.results).then(() => {
+          GetProductMarketPrice(results.results).then(() => {
             this.setState({ loading: false });
           });
         } else {
@@ -275,13 +214,13 @@ class CardDetail extends Component {
         <div className="details">
           {this.props.cardArr.length > 1 ? (
             this.state.page === 0 ? (
-              <Button disabled style={{ background: "none" }}>
+              <Button disabled style={{ background: 'none' }}>
                 <Icon name="arrow left" />
               </Button>
             ) : (
               <Button
-                onClick={event => this.handleClick(event, "previous")}
-                style={{ background: "none" }}
+                onClick={event => this.handleClick(event, 'previous')}
+                style={{ background: 'none' }}
               >
                 <Icon name="arrow left" />
               </Button>
@@ -302,9 +241,9 @@ class CardDetail extends Component {
           </div>
           <div
             style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between"
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
             }}
           >
             <div>
@@ -364,13 +303,13 @@ class CardDetail extends Component {
           </div>
           {this.props.cardArr.length > 1 ? (
             this.state.page === this.props.cardArr.length - 1 ? (
-              <Button disabled style={{ background: "none" }}>
+              <Button disabled style={{ background: 'none' }}>
                 <Icon name="arrow right" />
               </Button>
             ) : (
               <Button
-                onClick={event => this.handleClick(event, "next")}
-                style={{ background: "none" }}
+                onClick={event => this.handleClick(event, 'next')}
+                style={{ background: 'none' }}
               >
                 <Icon name="arrow right" />
               </Button>
@@ -405,10 +344,10 @@ class CardDetail extends Component {
         </div>
         <Divider />
         <div>
-          <div style={{ display: "flex" }}>
-            <div style={{ paddingRight: "10px" }}>{legalities}</div>
-            <div style={{ paddingRight: "10px" }}>{rulings}</div>
-            <div style={{ paddingRight: "10px", width: "180px" }}>{cost}</div>
+          <div style={{ display: 'flex' }}>
+            <div style={{ paddingRight: '10px' }}>{legalities}</div>
+            <div style={{ paddingRight: '10px' }}>{rulings}</div>
+            <div style={{ paddingRight: '10px', width: '180px' }}>{cost}</div>
           </div>
         </div>
       </div>
